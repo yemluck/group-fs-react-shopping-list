9c3bf297f88b0a2aa7bc962ad04d3cd5b8675eb3
Chris Agbolade <yemluck@gmail.com>
2022-01-13 10:32:53 -0600

    changes made to database

--- database.sql
+++ database.sql
@@ -5,13 +5,9 @@ CREATE TABLE groceries (
 	id SERIAL PRIMARY KEY,
 	name VARCHAR(80) NOT NULL,
 	quantity FLOAT(20) NOT NULL,
-	unit INT NOT NULL,
+	unit VARCHAR(20) NOT NULL,
 	purchased BOOLEAN default false
 );
 
 INSERT INTO groceries(name, quantity, unit)
 VALUES ('tomatoes', 4.3, 2),('bread',5, 2);
-
-SELECT * FROM groceries;
-
-DROP TABLE groceries;
